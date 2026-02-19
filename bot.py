9e655b019c4b9691a760b5a8ea152f90a8168f20
alennn2111-spec <alennn2111@gmail.com>
2026-02-19 22:34:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,10 +3,10 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = os.environ["TOKEN"]
-BASE_URL = os.environ["RENDER_EXTERNAL_URL"]
+URL = os.environ["RENDER_EXTERNAL_URL"]
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot is alive 🚀")
+    await update.message.reply_text("I am alive 🚀")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
@@ -15,6 +15,7 @@ if __name__ == "__main__":
     app.run_webhook(
         listen="0.0.0.0",
         port=int(os.environ.get("PORT", 10000)),
-        webhook_url=f"{BASE_URL}/webhook"
+        webhook_url=f"{URL}/webhook"
     )
+
 
